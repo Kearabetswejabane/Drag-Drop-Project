cb445807b3699f0b273f30e5f16954e3006372f9
Kearabetswe <keahjabane@gmail.com>
2025-02-25 09:02:26 +0200

    Added DOM Selection and OOP Rendering

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log('kearabetswe');
-console.log('Wavhudi');
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const prjInput = new ProjectInput();
